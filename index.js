6dfc2f5e1205bbde983719963eb3e66f51a44e5b
rond6251 <rondom@microsoft.com>
2017-09-20 18:09:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello MS World!");
+    response.end("Hello World!");
 
 });
 
